53c5299bd62c51c15b33acca11714602665cde81
Lodraie <74671908+Lodraie@users.noreply.github.com>
2020-11-25 17:41:51 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+int main()
+{
+    int decompte = 0
+    int c = 0 ;
+    while (decompte = 0) {
+        printf("Choississez une option : \n 1. IA facile. ; \n 2. IA moyenne. ; \n 3. joueur contre joueur. \n 4. quitter le programme.") ;
+        scanf(c);
+        if c = 1 {
+            //démarrer le programme Facile
+        }
+        else if c = 2 {
+            //démarrer le programme Moyen
+        }
+        else if c = 3 {
+            //démarrer le programme Versus
+        }
+        else if c = 4{
+            printf("fin du programme.");
+            decompte = 1 ;
+        }
+        else {
+            printf("Erreur : veuillez choisir une option disponible");
+        }
+    }
+}
+    //Tableau T1;
+    //GenererTab1(T1);
+    //afficher(T1);
+}

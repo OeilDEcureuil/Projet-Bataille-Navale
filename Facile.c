a27c040f8780c644b70f60bca453d26a6f08a372
Lodraie <74671908+Lodraie@users.noreply.github.com>
2020-11-25 18:45:44 +0100

    Create Facile.c

---
+++ Facile.c
@@ -0,0 +1,25 @@
+#include "tableau.h"
+#include "Facile.h"
+#include "Tour_joueur"
+
+//Non fini ni utilisable pour le moment 
+
+void facile()
+{
+    Tableau T1;
+    int t = 1;
+    GenererTab1(T1);
+    afficher(T1);
+    saisirbateaux(T1);
+    Tableau T2;
+    printf("l'IA place ses bateaux...")
+    //placer aléatoirement les bateaux sur le plateau
+    while(int t = 1; t != 0; t++){
+        if (t%2 == 0){
+            tour_joueur();
+        }
+        else {
+            rand()
+        }
+    }
+}

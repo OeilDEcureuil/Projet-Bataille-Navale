db1df13782e81ceef5738cf27776b987190bb9f0
OeilDEcureuil <74672799+OeilDEcureuil@users.noreply.github.com>
2020-11-25 18:36:02 +0100

    Update SaisirBateaux.c

--- SaisirBateaux.c
+++ SaisirBateaux.c
@@ -1,7 +1,7 @@
 #include "SaisirBateaux.h"
 
 
-void saisirbateaux1()
+void saisirbateaux1(Tableau T1)
 {
     int x, y, z;
     printf("Veuillez chosir l'emplacement de votre bateau. \n\n");
@@ -11,9 +11,58 @@ void saisirbateaux1()
     printf("Position verticale du bateau :");
     scanf("%d", &y);
     printf("Choisir la direction du bateau \n\n");
-    printf("1.  Haut \n");
-    printf("2.  Droite \n");
-    printf("3.  Bas \n");
-    printf("4.  Gauche \n");
+
+    if (T1[x-1][y] == NULL )
+        {
+            printf("1.  Haut \n");
+            printf("2.  Droite \n");
+            printf("3.  Bas \n");
+        }
+        else if (T1[x+1][y] == NULL )
+            {
+                printf("1.  Haut \n");
+                printf("3.  Bas \n");
+                printf("4.  Gauche \n");
+            }
+            else if (T1[x][y-1] == NULL )
+                {
+                    printf("1.  Haut \n");
+                    printf("2.  Droite \n");
+                    printf("4.  Gauche \n");
+                }
+                else if (T1[x][y+1] == NULL )
+                    {
+                        printf("2.  Droite \n");
+                        printf("3.  Bas \n");
+                        printf("4.  Gauche \n");
+                    }
+                    else if (T1[x-1][y-1] == NULL )
+                        {
+                            printf("2.  Droite \n");
+                            printf("3.  Bas \n");
+                        }
+                        else if (T1[x-1][y+1] == NULL )
+                            {
+                                printf("1.  Haut \n");
+                                printf("2.  Droite \n");
+                            }
+                            else if (T1[x+1][y-1] == NULL )
+                                {
+                                   printf("3.  Bas \n");
+                                   printf("4.  Gauche \n");
+                                }
+                                else if (T1[x+1][y+1] == NULL )
+                                    {
+                                        printf("1.  Haut \n");
+                                        printf("4.  Gauche \n");
+                                    }
+                                    else    printf("1.  Haut \n");
+                                            printf("2.  Droite \n");
+                                            printf("3.  Bas \n");
+                                            printf("4.  Gauche \n");
+
+
+
+
 
 }

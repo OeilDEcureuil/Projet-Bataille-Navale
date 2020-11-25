c3da2cf1e7bd23c2131559d630b722bbf07b1218
OeilDEcureuil <74672799+OeilDEcureuil@users.noreply.github.com>
2020-11-25 18:07:50 +0100

    Add files via upload

---
+++ SaisirBateaux.c
@@ -0,0 +1,19 @@
+#include "SaisirBateaux.h"
+
+
+void saisirbateaux1()
+{
+    int x, y, z;
+    printf("Veuillez chosir l'emplacement de votre bateau. \n\n");
+    printf("Position horizontale du bateau :");
+    scanf("%d", &x);
+    printf("\n");
+    printf("Position verticale du bateau :");
+    scanf("%d", &y);
+    printf("Choisir la direction du bateau \n\n");
+    printf("1.  Haut \n");
+    printf("2.  Droite \n");
+    printf("3.  Bas \n");
+    printf("4.  Gauche \n");
+
+}

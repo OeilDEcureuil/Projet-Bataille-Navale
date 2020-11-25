7fedd4039c14849483eff79d41a91a90d0881a35
Lodraie <74671908+Lodraie@users.noreply.github.com>
2020-11-25 17:45:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
+#include "Facile.h"
+#include "Moyen.h"
+#include "Versus.h"
+#include "tableau.h"
+
+
 int main()
 {
-    int decompte = 0
+    int decompte = 0;
     int c = 0 ;
     while (decompte = 0) {
         printf("Choississez une option : \n 1. IA facile. ; \n 2. IA moyenne. ; \n 3. joueur contre joueur. \n 4. quitter le programme.") ;
         scanf(c);
-        if c = 1 {
+        if (c = 1) {
             //démarrer le programme Facile
         }
-        else if c = 2 {
+        else if (c = 2) {
             //démarrer le programme Moyen
         }
-        else if c = 3 {
+        else if (c = 3) {
             //démarrer le programme Versus
         }
-        else if c = 4{
+        else if (c = 4){
             printf("fin du programme.");
             decompte = 1 ;
         }
@@ -22,7 +28,7 @@ int main()
             printf("Erreur : veuillez choisir une option disponible");
         }
     }
-}
+
     //Tableau T1;
     //GenererTab1(T1);
     //afficher(T1);

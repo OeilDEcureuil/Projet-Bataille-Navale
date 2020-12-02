95c3319d46cd46ecc60e2de1ff74f8e5086c9f04
Lodraie <74671908+Lodraie@users.noreply.github.com>
2020-12-02 17:26:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "Facile.h"
 #include "Moyen.h"
 #include "Versus.h"
 #include "tableau.h"
 
 
-int main()
-{
+void main(){
     int decompte = 0;
-    int c = 0 ;
-    while (decompte = 0) {
-        printf("Choississez une option : \n 1. IA facile. ; \n 2. IA moyenne. ; \n 3. joueur contre joueur. \n 4. quitter le programme.") ;
-        scanf(c);
-        if (c = 1) {
+    int c ;
+    while (decompte == 0) {
+        printf("Choississez une option : \n 1. IA facile. ; \n 2. IA moyenne. ; \n 3. joueur contre joueur. \n 4. quitter le programme. \n \n ") ;
+        scanf("%d", &c);
+        if (c == 1) {
+            printf("Vous avez choisis : contre IA en facile. \n");
             //démarrer le programme Facile
         }
-        else if (c = 2) {
+        else if (c == 2) {
+            printf("Vous avez choisis : contre IA en moyen. \n");
             //démarrer le programme Moyen
         }
-        else if (c = 3) {
+        else if (c == 3) {
+            printf("Vous avez choisis : joueur contre joueur. \n");
             //démarrer le programme Versus
         }
-        else if (c = 4){
-            printf("fin du programme.");
+        else if (c == 4){
+            printf("fin du programme. \n");
             decompte = 1 ;
         }
         else {
-            printf("Erreur : veuillez choisir une option disponible");
+            printf("Erreur : veuillez choisir une option disponible. \n");
         }
     }
 
@@ -33,3 +37,4 @@ int main()
     //GenererTab1(T1);
     //afficher(T1);
 }
+

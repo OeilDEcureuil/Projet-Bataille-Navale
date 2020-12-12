24c1326054d22d7bdb843958fc69299d01c1d455
OeilDEcureuil <74672799+OeilDEcureuil@users.noreply.github.com>
2020-12-12 19:08:03 +0100

    Update Bateau.h

--- Bateau.h
+++ Bateau.h
@@ -3,6 +3,13 @@
 
 typedef char Tableau[10][10];
 
-int PositionBateaux(Tableau T1, int x, int y);
+int PositionBateaux( Tableau T1, int t, int *x, int *y, int *z);
 
-void DirectionBateau(Tableau T1, int x, int y);
+int bateau2case(Tableau T1, int x, int y);
+
+int bateau3case(Tableau T1, int x, int y);
+
+int bateau4case(Tableau T1, int x, int y);
+
+int bateau5case(Tableau T1, int x, int y);
+

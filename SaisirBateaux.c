2bea56ad6b1f1727f8f1788f44fab2fa0394f58c
Lodraie <74671908+Lodraie@users.noreply.github.com>
2020-12-14 15:05:17 +0100

    Update SaisirBateaux.c

--- SaisirBateaux.c
+++ SaisirBateaux.c
@@ -1,158 +1,108 @@
-void saisirbateaux1(Tableau T1)
+#include "tableau.h"
+#include "SaisirBateau.h"
+
+void saisirbateaux1(Tableau T1, int t)
 {
-    int x, y, z;
-    printf("Veuillez chosir l'emplacement de votre bateau. \n\n");
-    printf("Position horizontale du bateau :");
-    scanf("%d", &x);
-    printf("\n");
-    printf("Position verticale du bateau :");
-    scanf("%d", &y);
-    printf("Choisir la direction du bateau \n\n");
-
-    if (T1[x-1][y] != '  ~' )
-        {
-            printf("1.  Haut \n");
-            printf("2.  Droite \n");
-            printf("3.  Bas \n");
-        }
-        else if (T1[x+1][y] != '  ~' )
-            {
-                printf("1.  Haut \n");
-                printf("3.  Bas \n");
-                printf("4.  Gauche \n");
-            }
-            else if (T1[x][y-1] == NULL )
-                {
-                    printf("1.  Haut \n");
-                    printf("2.  Droite \n");
-                    printf("4.  Gauche \n");
-                }
-                else if (T1[x][y+1] == NULL )
-                    {
-                        printf("2.  Droite \n");
-                        printf("3.  Bas \n");
-                        printf("4.  Gauche \n");
-                    }
-                    else if (T1[x-1][y-1] == NULL )
-                        {
-                            printf("2.  Droite \n");
-                            printf("3.  Bas \n");
-                        }
-                        else if (T1[x-1][y+1] == NULL )
-                            {
-                                printf("1.  Haut \n");
-                                printf("2.  Droite \n");
-                            }
-                            else if (T1[x+1][y-1] == NULL )
-                                {
-                                   printf("3.  Bas \n");
-                                   printf("4.  Gauche \n");
-                                }
-                                else if (T1[x+1][y+1] == NULL )
-                                    {
-                                        printf("1.  Haut \n");
-                                        printf("4.  Gauche \n");
-                                    }
-                                    else    printf("1.  Haut \n");
-                                            printf("2.  Droite \n");
-                                            printf("3.  Bas \n");
-                                            printf("4.  Gauche \n");
-
-
-
-
-
-}
-
-
-
-
-//V2 
-
-void saisirbateaux1(Tableau T1, int t)  
-{
-    int x, y, z, i, G, D, H, B;
+    int x, y, z, i, G, D, H, B, compteur, activation;
     i = 1;
     G = 2;
     D = 2;
     H = 2;
     B = 2;
-    printf("Veuillez chosir l'emplacement de votre bateau. \n\n");
-    printf("Position horizontale du bateau :");
-    scanf("%d", &x);
-    printf("\n");
-    printf("Position verticale du bateau :");
-    scanf("%d", &y);
-    printf("Vous avez choisi : %d %d \n", x, y) ;
-    printf("Choisir la direction du bateau \n\n");
+    activation = 0;
+    compteur = 0;
+    while (activation == 0)
+    {
+        printf("Veuillez choisir l'emplacement de votre bateau. \n\n");
+        printf("Position verticale du bateau :");
+        scanf("%d", &x);
+        printf("\n");
+        printf("Position horizontale du bateau :");
+        scanf("%d", &y);
+        printf("Vous avez choisi : %d %d \n", x, y) ;
+        printf("Choisir la direction du bateau \n\n");
 
-    if (y+1-t >= 0)
-    {
-        G = 1;
-    }
-    i = 1;
-    while (i < t)
-    {
-        if (T1[x][y+1-i] == 'H')
+        if (y+1-t >= 0)
         {
-            G = 2;
+            G = 1;
         }
-        i = i+1;
-    }
-    if (x+1-t >= 0)
-    {
-        H = 1;
-    }
-    i = 1;
-    while (i < t)
-    {
-        if (T1[x+1-i][y] == 'H')
+        i = 1;
+        while (i < t)
         {
-            H = 2;
+            if (T1[x][y+1-i] == 'H')
+            {
+                G = 2;
+            }
+            i = i+1;
         }
-        i = i+1;
-    }
-    if (y-1+t <=10 )
-    {
-        D = 1;
-    }
-    i = 1 ;
-    while (i < t)
-    {
-        if (T1[x][y-1+i] == 'H')
+        if (x+1-t >= 0)
+            {
+                H = 1;
+            }
+        i = 1;
+        while (i < t)
         {
-            D = 2;
+            if (T1[x+1-i][y] == 'H')
+            {
+                H = 2;
+            }
+            i = i+1;
         }
-        i = i+1;
-    }
-    if (x-1+t <= 10 )
-    {
-        B = 1;
-    }
-    i = 1 ;
-    while (i < t)
-    {
+        if (y+1+t <=10 )
+        {
+                D = 1;
+            }
+        i = 1 ;
+        while (i < t)
+        {
+            if (T1[x][y-1+i] == 'H')
+            {
+                D = 2;
+            }
+            i = i+1;
+        }
+        if (x+1+t <= 10 )
+        {
+            B = 1;
+        }
+        i = 1 ;
+        while (i < t)
+        {
         if (T1[x-1+i][y] == 'H')
         {
             B = 2;
         }
         i = i +1;
-    }
-    if (H == 1)
-    {
-        printf("1.  Haut \n");
-    }
-    if (G == 1)
-    {
-        printf("2.  Gauche \n");
-    }
-    if (D == 1)
-    {
-        printf("3.  Droite \n");
-    }
-    if (B == 1)
-    {
-        printf("4.  Bas \n");
+        }
+        if (H == 1)
+        {
+            printf("1.  Haut \n");
+            compteur = compteur + 1 ;
+        }
+        if (G == 1)
+        {
+            printf("2.  Gauche \n");
+            compteur = compteur + 1 ;
+        }
+        if (D == 1)
+        {
+            printf("3.  Droite \n");
+            compteur = compteur + 1 ;
+        }
+        if (B == 1)
+        {
+            printf("4.  Bas \n");
+            compteur = compteur + 1 ;
+        }
+        if (compteur == 0)
+        {
+            printf("erreur : bateau implassable.");
+
+        }
+            else
+            {
+                activation = 1;
+            }
     }
     scanf("%d", &z);
     i = 0;
@@ -188,9 +138,6 @@ void saisirbateaux1(Tableau T1, int t)
             i = i+1;
         }
     }
-
-
-
 
 
     afficher(T1);
